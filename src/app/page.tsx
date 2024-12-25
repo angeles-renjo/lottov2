97ef6ab102a9b7d4d78ed1b5e6e5ad9af0960e19
angeles-renjo <82358240+angeles-renjo@users.noreply.github.com>
2024-12-25 15:09:40 +0800

    fix v3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import LottoDisplay from "@/components/LottoDisplay";
 export default function Home() {
   return (
     <main className="p-6">
-      <h1 className="text-4xl font-bold text-green-600 mb-6">
-        PCSO Lotto Results
-      </h1>
+      <h1 className="text-4xl font-bold mb-6">PCSO Lotto Results</h1>
       <LottoDisplay />
     </main>
   );
